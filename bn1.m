ada8fe06a2b44e188370b5d33af33d7442990826
Zarebski <Alex@Alex-Zarebskis-MacBook-Pro.local>
2015-05-30 11:47:56 +1000

    write some code to show off how to incorporate soft evidence and how to compute the most probable states given some evidence.

--- bn1.m
+++ bn1.m
@@ -18,7 +18,7 @@ dag(S,W)=1;
 draw_graph(dag, {'Cloudy','Sprinkler','Rain','Wet grass'});
 % and put a label on out plot
 title('Hello world!');
-
+close;
 % the bnt function make_layout returns coordinate vectors for the graph 
 % produced by draw_graph  
 % [X,Y] = make_layout(dag);
@@ -73,7 +73,7 @@ marg = marginal_nodes(engine, S);   % compute the marginal distribution of
                                     % the variable S 
 p_g = marg.T; % define p as the prob the sprinkler is on
 
-fprintf('P(Srinkler=on|Grass=wet) = %f\n',p_g(2));
+fprintf('\nP(Srinkler=on|Grass=wet) = %f\n',p_g(2));
 
 % compute the probability given the additional evidence that it is raining
 evidence{R} = 2;
@@ -89,7 +89,7 @@ subplot(1,2,1), bar(p_g);
 title('Given grass wet'), axis([0.5,2.5,0,1]);
 subplot(1,2,2), bar(p_gr);
 title('Given grass wet and raining'), axis([0.5,2.5,0,1]);
-close
+close;
 
 %-------------------------------------------------------------------------%
 % Joint distributions
@@ -100,7 +100,7 @@ evidence = cell(1,N);   % reset evidence
 m = marginal_nodes(engine, [S R W]);    % marginal distributions of S, R, W
 
 % examples of looking up values in the joint distribution
-fprintf('P(Sprinkler=off,Raining=false,Grass=wet) = %f\n',m.T(1,1,2));
+fprintf('\nP(Sprinkler=off,Raining=false,Grass=wet) = %f\n',m.T(1,1,2));
 fprintf('P(Sprinkler=on,Raining=false,Grass=wet) = %f\n',m.T(2,1,2));
 
 % update the evidence due to observation that Raining=true
@@ -110,6 +110,50 @@ m = marginal_nodes(engine, [S R W], 1); % the 1 needs to be added to get
                                         % the full table of values
 
 % look at the same examples
-fprintf('P(Sprinkler=off,Raining=false,Grass=wet|Raining=true) = %f\n',m.T(1,1,2));
+fprintf('\nP(Sprinkler=off,Raining=false,Grass=wet|Raining=true) = %f\n',m.T(1,1,2));
 fprintf('P(Sprinkler=on,Raining=false,Grass=wet|Raining=true) = %f\n',m.T(2,1,2));
 
+%-------------------------------------------------------------------------%
+% Soft evidence
+%-------------------------------------------------------------------------%
+
+% soft evidence refers to evidence which does not instantiate a variable
+% but gives a distribution on the variable
+
+% consider the case where the grass looks wet but we don't know for sure so
+% we say that there is a 75% chance that the grass is wet then compute the
+% probability that it is raining
+
+evidence = cell(1,N);       % no variables instantiated
+soft_evidence = cell(1,N);  % preallocate soft_evidence
+soft_evidence{W} = [0.25,0.75];  % distribution on W
+
+% put this evidence into the engine
+[engine, ~] = enter_evidence(engine, evidence, 'soft', soft_evidence);
+
+m = marginal_nodes(engine, R);
+
+fprintf('\nP(Raining=true|soft evidence on W) = %f\n',m.T(2));
+
+% now assume that we know it is cloudy
+evidence{C} = 2;
+[engine, ~] = enter_evidence(engine, evidence, 'soft', soft_evidence);
+m = marginal_nodes(engine, R);
+fprintf('\nP(Raining=true|Cloudy=true,soft evidence on W) = %f\n',m.T(2));
+
+%-------------------------------------------------------------------------%
+% Most probable explanation
+%-------------------------------------------------------------------------%
+
+% in the spirit of maximum likelihood estimation one might want to compute
+% the most probable explanation of the evidence this takes the form of an
+% instantiation of the variables which is most likely given the evidence
+
+evidence = cell(1,N);
+[mpe, ~] = calc_mpe(engine, evidence, 1);
+fprintf('\nThe prior most probable explanation is\n');
+disp(mpe);
+evidence{S} = 2;
+[mpe, ~] = calc_mpe(engine, evidence, 1);
+fprintf('\nThe given that the sprinkler is on the most probable explanation is\n');
+disp(mpe);
